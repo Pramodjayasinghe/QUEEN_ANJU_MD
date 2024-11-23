6487810a2af04288425eee6340774568c78c63d0
Pramodjayasinghe <Pramodjayasinghe428@gmail.com>
2024-11-23 21:45:20 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qU9HBB6S#5QQ3KdZfCZkoGnetA75HLgjVlaDt3ygfEsgmNMHK_gs",
 };
